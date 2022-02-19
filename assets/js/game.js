f1d7380be90c6dc6313ba5e78cfa7942758278a1
Jared Ruiz <jared707@gmail.com>
2022-02-19 07:16:07 -0600

    Finalized highscore API integration.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,8 @@
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//      *fights all enemy-robots
-//      *Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
+//////////////////////////////////////////////
+// robot-gladiator text based browser game //
+////////////////////////////////////////////
 
+//check if player wants to fight or skip
 var fightOrSkip = function() {
     //ask player if they'd like to fight or skip using fightOrSkip function
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -135,15 +134,26 @@ var startGame = function() {
 
 //function to end the entire game
 var endGame = function() {
-    //if player is still alive, the player wins
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ",");
-    }
-    else
-    {
-        window.alert("You've lost your robot in battle.");
+    window.alert("The game has ended. Let's see how you did!");
+
+    //check localStorage for highscore, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
     }
 
+    //if player has more money than the highscore, player has new highscore!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+    
+    //ask player if they would like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
@@ -251,11 +261,6 @@ var enemyInfo = [
         attack: randomNumber(10, 14),
     }    
 ];
-
-console.log(enemyInfo);
-console.log(enemyInfo[0]);
-console.log(enemyInfo[0].name);
-// console.log(enemyInfo[0]['attack']);
 
 //starts game upon page load
 startGame();
